d6423ccbba8ea545afe8bdf318cea31e845206f3
Voste254 <onyangosteveok@gmail.com>
2025-05-27 17:11:11 +0300

    Modifications

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Navbar from './components/landing-page/Navbar';
 import HeroSection from './components/landing-page/Herosection';
 import Footer from './components/landing-page/Footer';
 import Services from './components/landing-page/Services';
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/landing-page/Login';
 
 function Dashboard(){
   return(
@@ -18,10 +20,22 @@ function Dashboard(){
 function Landingpage(){
   return(
     <>
-    <Navbar/>
-    <HeroSection/>
-    <Services/>
-    <Footer/>
+    
+    <Routes>
+      <Route path='/' element={<>
+      <Navbar/>
+      <HeroSection/>
+      <Services/>
+        <Footer/>
+      </>} >
+                    
+      </Route>
+      <Route exact path='/login' element={<Login/>} />
+      <Route  path='/dashboard' element={<Dashboard/>} />
+
+
+    </Routes>    
+  
     </>
     
   )
@@ -30,8 +44,7 @@ function App() {
   return (
     <div className="App">
       
-      <Landingpage/>
-      
+      <Landingpage/>     
       
     </div>
   );
--- src/components/landing-page/Login.jsx
+++ src/components/landing-page/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
   return (
     <div> 
         <h1>This is the login page</h1>
+        <Link to='/dashboard'>
+         <button>Login</button>
+        </Link>
+       
     </div>
   )
 }
